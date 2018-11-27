3a82c9ff54775c466acd34d1b36548cc3552fd38
DelBiss <philrousse@gmail.com>
2018-11-27 13:24:00 -0500

    Add functionnality to Grid function: - Grid size can be a proportion of sheet size + Grid can now have Main line, base on a function. - Grid can now have a title. Add functionnality to Util function: + Add a Txt image fct. - Save function put a white border around img - Compose function can now put space between img (default: 5) - Simplify Border function

--- grid.py
+++ grid.py
@@ -18,10 +18,10 @@ def CreateLineAcross(img, p, fillColor=(0,)*3, width=1, pattern=0, IsVertical=Fa
         LineLenght = img.size[1]
     else:
         LineLenght = img.size[0]
+    
     pixelLines = []
-
-    basePattern = PatternDef[pattern]
     baseLines = []
+    basePattern = PatternDef[pattern]
     for linePattern in basePattern:
         baseLines.append(
             (linePattern*((LineLenght//len(linePattern))+1))[:LineLenght])
@@ -31,7 +31,6 @@ def CreateLineAcross(img, p, fillColor=(0,)*3, width=1, pattern=0, IsVertical=Fa
     basePos = p
 
     for pos1, pl in enumerate(pixelLines):
-
         for pos2, pv in enumerate(pl):
             if pv:
                 if IsVertical:
@@ -41,26 +40,33 @@ def CreateLineAcross(img, p, fillColor=(0,)*3, width=1, pattern=0, IsVertical=Fa
 
                 img.putpixel(finalPos, fillColor)
 
-    pass
-
-
-def GetGrid(gridDimension=(26, 30), lineColor=(0,)*3, Pattern=0):
-    maxSize = (2400, 3000)
+def GetGrid(gridDimension=(26, 30), lineColor=(0,)*3, Pattern=0, title="Grid", pageProportion=(1, 1), mainLine=[lambda x: False]):
+    maxSize = (int(2400*pageProportion[0]), int(3000*pageProportion[1]))
     borderSize = 5
+    Width = 2
+
     gridSize = min([x[0]//x[1] for x in zip(maxSize, gridDimension)])
     gridImgSize = tuple(map(lambda x: x * gridSize, gridDimension))
-    grid = Image.new('RGB', gridImgSize, color=(255, 255, 255))
+    grid = Image.new('RGBA', gridImgSize, color=(255, 255, 255, 255))
 
-    Width = 2
-    for i in range(1, gridDimension[0]):
-        left = i*gridSize
-        CreateLineAcross(grid, left, lineColor, Width, Pattern, True)
+    if len(mainLine) == 1:
+        mainLine = mainLine*2
 
-    for i in range(1, gridDimension[1]):
-        top = i*gridSize
-        CreateLineAcross(grid, top, lineColor, Width, Pattern, False)
+    for ori, d in enumerate(gridDimension):
+        for i in range(1, d):
+            stop = i*gridSize
+            lWidth = Width
+            if mainLine[ori](i):
+                lWidth = Width*2
+            CreateLineAcross(grid, stop, lineColor, lWidth, Pattern, ori)
 
-    return util.img.AddBorder(grid, borderSize)  # im#im
+
+    grid = util.img.AddBorder(grid, borderSize)
+
+    if len(title) > 0:
+        t1 = util.img.GetTxtImg(title, (grid.size[0], 100))
+        grid = util.img.ComposeImg(t1, grid)
+    return grid   # im#im
 
 
 def Test():
@@ -69,25 +75,57 @@ def Test():
 
     def NoTest():
         patternRange = range(6)
+        allImg = []
+        resizePattern = 5
+        mainLine = [lambda x: x%4==0]
         for p in patternRange:
-            util.img.Save(GetGrid((55, 70), (0,)*3, p), "gridTest{}".format(p))
+            patImg = []
+            for c in [("Black", (0,)*3, (127,)*3), ("Grey", (127,)*3, (0,)*3)]:
+                l = "{} - pattern {}".format(c[0], p)
+
+                #grid = util.img.ComposeImg(t1,grid)
+                grid = GetGrid((12, 12), c[1], p, "", (1/2, 1/3),mainLine=mainLine)
+                t1 = util.img.GetTxtImg(l, (grid.size[0]//2, 100))
+                im1 = Image.new(
+                    'RGBA', ((grid.size[0]//2)//resizePattern, 80//resizePattern), (255,)*4)
+                CreateLineAcross(im1, 0, c[1], 80//resizePattern, p)
+                im1 = im1.resize(
+                    (im1.size[0]*resizePattern, im1.size[1]*resizePattern))
+                im1 = util.img.AddBorder(im1, 1, c[2])
+                t1 = util.img.ComposeImg(t1, im1, IsV=False)
+                grid = util.img.ComposeImg(t1, grid)
+                patImg.append(grid)
+            #im2 = GetGrid((12, 12), (127,)*3, p,"Grey - pattern {}".format(p),(1/2,1/3))
+            allImg.append(util.img.ComposeImg(*patImg, IsV=False))
+        full = util.img.ComposeImg(*(allImg))
+        #full = util.img.AddBorder(full,100,(255,255,255,255))
+        util.img.Save(full, "DifGrid", (300, 300), "grid/", True)
 
     def Line_Test():
-        import itertools
-        distanceBetween = 20
-        fillColor = (128,)*3
-        widthRange = range(5)
+        
+        fillColor = (127,)*3
+        #widthRange = range(5)
         patternRange = range(6)
-        all_line = list(itertools.product(patternRange, widthRange))
-        print("Drawing {} lines".format(len(all_line)))
-        im = Image.new('RGB', (distanceBetween*(len(all_line)+2),)*2, (255,)*3)
-        for isV in [False, True]:
-            for i, v in enumerate(all_line):
-                p, w = v
-                CreateLineAcross(im, (i+1)*distanceBetween,
-                                 fillColor, w+1, p, isV)
-        util.img.Save(im, "LineTest")
+        allImg = []
+        for p in patternRange:
+            im1 = Image.new('RGBA', (10, 10), (255,)*4)
+            im2 = Image.new('RGBA', (10, 10), (255,)*4)
+            CreateLineAcross(im1, 0, (0,)*3, 10, p)
+            CreateLineAcross(im2, 0, fillColor, 10, p)
+            imr1 = im1.resize((im1.size[0]*3, im1.size[1]*3))
+            imr2 = im2.resize((im2.size[0]*3, im2.size[1]*3))
+            im1 = util.img.AddBorder(im1, 1, fillColor)
+            imr1 = util.img.AddBorder(imr1, 1, fillColor)
+            im2 = util.img.AddBorder(im2, 1)
+            imr2 = util.img.AddBorder(imr2, 1)
+            im1 = util.img.ComposeImg(imr1, im1, IsV=False)
+            im2 = util.img.ComposeImg(imr2, im2, IsV=False)
+            allImg.append(util.img.ComposeImg(im1, im2))
+        full = util.img.ComposeImg(*(allImg))
+        util.img.Save(full, "LineTest", (300, 300), "grid/", True, border=0)
+
     Launcher.Launch(NoTest)
+    Launcher.Launch(Line_Test)
 
 
 if __name__ == "__main__":
